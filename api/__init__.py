1a391bd9ae420b549acc7f9682e0f1c901192c09
yora3vs3 <99631524+yora3vs3@users.noreply.github.com>
2022-10-23 00:18:14 +0300

    I love APIS

--- api/__init__.py
+++ api/__init__.py
@@ -1,14 +1,15 @@
-# Copyright 2022 user
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/usr/bin/python3
+"""This module define a blueprint for routes with Blueprint object"""
+from flask import Blueprint
 
+
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,37 +1,22 @@
 #!/usr/bin/python3
-"""
-Flask route that returns json status response
-"""
-from api.v1.views import app_views
-from flask import jsonify, request
+"""This module implement a rule that returns the status of the application"""
+from flask import jsonify
 from models import storage
+from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route("/status", strict_slashes=False)
 def status():
-    """
-    function for status route that returns the status
-    """
-    if request.method == 'GET':
-        resp = {"status": "OK"}
-        return jsonify(resp)
+    """View function that return a json message"""
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route("/stats", strict_slashes=False)
 def stats():
-    """
-    function to return the count of all class objects
-    """
-    if request.method == 'GET':
-        response = {}
-        PLURALS = {
-            "Amenity": "amenities",
-            "City": "cities",
-            "Place": "places",
-            "Review": "reviews",
-            "State": "states",
-            "User": "users"
-        }
-        for key, value in PLURALS.items():
-            response[value] = storage.count(key)
-        return jsonify(response)
+    """function that retrieves the number of each object by type"""
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
